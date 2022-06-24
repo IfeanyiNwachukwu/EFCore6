afbe22b085e1d56717ec90782275dd49fa3e0389
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-06-24 12:13:40 +0100

    Added an EfCore Db Library

--- Entity_Framework_Core_6/EFCore_Consolas/Entity_Framework_Core_Consolas/Configuration/AppSetup.cs
+++ Entity_Framework_Core_6/EFCore_Consolas/Entity_Framework_Core_Consolas/Configuration/AppSetup.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
 
 namespace Entity_Framework_Core_Consolas.Configuration
 {
-    internal class AppSetup
+    public static class AppSetup
     {
+        static IConfigurationRoot _configuration;
+     // public static DbContextOptionsBuilder<AdventureWorksContext> _optionsBuilder;
+
+        public static void BuildOptions()
+        {
+            _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+           // _optionsBuilder = new DbContextOptionsBuilder<AdventureWorksContext>();
+           // _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AdventureWorks"));
+        }
+
+        public static void DisplayConnectionString()
+        {
+            Console.WriteLine($"CNSTR: {_configuration.GetConnectionString("AdventureWorks")}");
+        }
     }
 }
---
+++ Entity_Framework_Core_6/EFCore_Consolas/Entity_Framework_Core_Db_Library/Entity_Framework_Core_Db_Library/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Entity_Framework_Core_Db_Library
+{
+    public class Class1
+    {
+
+    }
+}
