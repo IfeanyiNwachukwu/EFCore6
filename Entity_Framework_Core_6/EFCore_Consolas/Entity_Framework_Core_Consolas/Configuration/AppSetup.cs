6c99ea3bb33e012a9ca190ba52742f49118963f6
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-06-27 20:24:08 +0100

    Added Code to set u the database library and Model library

--- Entity_Framework_Core_6/EFCore_Consolas/Entity_Framework_Core_Consolas/Configuration/AppSetup.cs
+++ Entity_Framework_Core_6/EFCore_Consolas/Entity_Framework_Core_Consolas/Configuration/AppSetup.cs
@@ -1,17 +1,19 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Entity_Framework_Core_Db_Library.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Entity_Framework_Core_Consolas.Configuration
 {
     public static class AppSetup
     {
-        static IConfigurationRoot _configuration;
-     // public static DbContextOptionsBuilder<AdventureWorksContext> _optionsBuilder;
+         private static IConfigurationRoot _configuration;
+         public static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
 
         public static void BuildOptions()
         {
             _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
-           // _optionsBuilder = new DbContextOptionsBuilder<AdventureWorksContext>();
-           // _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AdventureWorks"));
+           _optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+           _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
         }
 
         public static void DisplayConnectionString()
---
+++ Entity_Framework_Core_6/EFCore_Consolas/Entity_Framework_Core_Db_Library/Entity_Framework_Core_Db_Library/DbContexts/InventoryDbContext.cs
@@ -0,0 +1,35 @@
+﻿using InventoryModels.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Entity_Framework_Core_Db_Library.DbContexts
+{
+    public class InventoryDbContext : DbContext
+    {
+        private static IConfigurationRoot _configuration;
+        public InventoryDbContext():base()
+        {
+
+        }
+        public InventoryDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                _configuration = builder.Build();
+
+                var cnstr = _configuration.GetConnectionString("InventoryManager");
+                optionsBuilder.UseSqlServer(cnstr);
+            }
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
